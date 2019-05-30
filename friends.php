edb791132a6ff86beca8dfb7f05f11585c3b2231
maruz-89 <zambranopmarina@gmail.com>
2019-05-29 23:45:32 -0300

    Subo archivos para todas las pantallas. Arranque con la pantalla de amigos y la cabecera de la aplicacion

---
+++ friends.php
@@ -0,0 +1,85 @@
+<?php
+$friendlist = [
+  [
+    "photo" => "profile_pic.jpg",
+    "name" => "Marina Zambrano",
+    "posts" => 12,
+    "followers" => 5,
+    "writings" => 2
+  ],
+  [
+    "photo" => "profile_pic.jpg",
+    "name" => "Fernanda Peralta",
+    "posts" => 13,
+    "followers" => 45,
+    "writings" => 1
+  ],
+  [
+    "photo" => "profile_pic.jpg",
+    "name" => "Marina Zambrano",
+    "posts" => 12,
+    "followers" => 5,
+    "writings" => 2
+  ],
+  [
+    "photo" => "profile_pic.jpg",
+    "name" => "Fernanda Peralta",
+    "posts" => 13,
+    "followers" => 45,
+    "writings" => 1
+  ],
+]
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+  <title>Friends</title>
+  </head>
+  <body>
+
+    <div class="friends_finder">
+      <h2>Escritores favoritos</h2>
+      <!-- Search form -->
+      <form class="form-inline md-form form-sm">
+        <input class="form-control form-control-sm auto w-75" type="text" placeholder="Busca un escritor">
+        <i class="fas fa-search" aria-hidden="true"></i>
+      </form>
+    </div>
+
+    <div class="friends_list">
+      <?php foreach ($friendlist as $friend): ?>
+      <div class="friend_container">
+        <div class="friend_photo">
+           <img src="img/<?= $friend["photo"] ?>" alt="Foto perfil">
+        </div>
+        <div class="friend_name"><?= $friend["name"] ?></div>
+        <div class="friend_information">
+          <ul>
+            <li class="posts"> <i class="fas fa-bell"></i>
+              <?php
+              echo " ". $friend["posts"];
+              if($friend["posts"] > 1)
+              {
+                echo " nuevos posteos";
+              }else {
+                echo " nuevo posteo";
+              }
+              ?>
+            </li>
+            <li class="followers"> <i class="fas fa-user-friends"></i> <?= " ".$friend["followers"] . " seguidores" ?> </li>
+            <li class="writings"> <i class="far fa-copy"></i> <?= " ".$friend["writings"] ." escritos" ?> </li>
+          </ul>
+        </div>
+        <button type="submit" name="view_profile" method="get">
+          Ver perfil
+        </button>
+      </div>
+    <?php endforeach; ?>
+    </div>
+  </body>
+</html>
