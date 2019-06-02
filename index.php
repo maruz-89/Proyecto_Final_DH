da3b5dfa636fe5f341b4d9950bd0adfd5d3447df
CarolinaTiraboschi <carotiraboschi@gmail.com>
2019-06-02 12:56:08 -0300

    Agrego código básico de index.php y register.php. Tuve problemas para linkear a CSS

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php  ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bienvenido</title>
+  </head>
+
+
+  <body>
+    <div class="container" style="margin-top:30px; margin-bottom: 30px;">
+        <div class="row justify-content-center">
+        <div class= "site-title">
+          <h1> SYZYGY </h1>
+          <h3>EL ENCUENTRO FORTUITO </h3>
+        </div>
+
+        <form method="post" enctype="multipart/form-data">
+  					<div class="row">
+  						<div class="col-sm-10">
+                <h2>INICIAR SESIÓN</h2>
+  							<div class="form-group">
+  								<input type="text" name="user" class="form-control" placeholder="Ingrese su usuario">
+                  <a href="">Olvidé mi usuario</a>
+
+
+                </div>
+                <div class="form-group">
+  								<input type="password" name="pass" class="form-control" placeholder="Contraseña">
+                  <a href="">Olvidé mi contraseña</a>
+
+                  <button type="submit" class="btn btn-primary">Registrarse</button>
+
+                </div>
+                <div class="col-12">
+                  <div class="register">
+                    <p>¿Todavía no estás registrado?</p>
+                    <a href="register.php">REGISTRARME AHORA </a>
+                  </div>
+
+
+
+
+  						</div>
+
+
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+
+  </body>
+</html>
---
+++ register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crea tu cuenta</title>
+  </head>
+  <body>
+    <div class="container" style="margin-top:30px; margin-bottom: 30px;">
+      <div class="row justify-content-center">
+      <div class= "site-title">
+        <h1> SYZYGY </h1>
+        <h3>EL ENCUENTRO FORTUITO </h3>
+      </div>
+
+      <form method="post" enctype="multipart/form-data">
+					<div class="row">
+						<div class="col-sm-10">
+              <h2>REGISTRARME</h2>
+              <div class="form-group">
+                <input type="text" name="user" class="form-control" placeholder="Nombre de Usuario">
+
+              </div>
+
+							<div class="form-group">
+								<input type="text" name="name" class="form-control" placeholder="Nombre Completo">
+
+              </div>
+              <div class="form-group">
+								<input type="email" name="email" class="form-control" placeholder="Correo Electrónico">
+
+              </div>
+              <div class="form-group">
+								<input type="password" name="pass" class="form-control" placeholder="Contraseña">
+
+              </div>
+              <div class="form-group">
+								<input type="password" name="pass-repeat" class="form-control" placeholder="Repita su contraseña">
+
+              </div>
+              <div class="col-12">
+
+							<button type="submit" class="btn btn-primary">Registrarse</button>
+						</div>
+
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+  </body>
+</html>
