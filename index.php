b7e945d3990ac4fd31e5f599788e97d257b588a4
CarolinaTiraboschi <carotiraboschi@gmail.com>
2019-06-03 23:42:58 -0300

    agrego versiones responsive de index y register

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 $section="LogIn";
 require_once("./partials/head.php");
 ?>
-
-  <body>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="css/styles.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crea tu cuenta</title>
+  </head>
+  <body class="galaxy">
     <div class="container" style="margin-top:30px; margin-bottom: 30px;">
         <div class="row justify-content-center">
         <div class= "site-title">
@@ -11,10 +19,14 @@ require_once("./partials/head.php");
           <h3>EL ENCUENTRO FORTUITO </h3>
         </div>
 
+<div class= " col-10 description">
+        <p>BREVE TEXTO DESCRIPTIVO DE LA APLICACIÓN. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incidid</p>
+      </div>
+
         <form method="post" enctype="multipart/form-data">
   					<div class="row">
-  						<div class="col-sm-10">
-                <h2>INICIAR SESIÓN</h2>
+  						<div class="col-10 formulario">
+                <h2 class= "registrar">INICIAR SESIÓN</h2>
   							<div class="form-group">
   								<input type="text" name="user" class="form-control" placeholder="Ingrese su usuario">
                   <a href="">Olvidé mi usuario</a>
@@ -25,19 +37,10 @@ require_once("./partials/head.php");
   								<input type="password" name="pass" class="form-control" placeholder="Contraseña">
                   <a href="">Olvidé mi contraseña</a>
 
-                  <button type="submit" class="btn btn-primary">Registrarse</button>
+                  <button type="submit" class="btn btn-primary">Ingresar</button>
 
                 </div>
-                <div class="col-12">
-                  <div class="register">
-                    <p>¿Todavía no estás registrado?</p>
-                    <a href="register.php">REGISTRARME AHORA </a>
-                  </div>
 
-
-
-
-  						</div>
 
 
 
@@ -45,10 +48,66 @@ require_once("./partials/head.php");
 
               </div>
             </div>
+
           </div>
+          </div>
+
+
         </div>
 
-
+        <div class="col-12 foot">
+            <p>¿Todavía no estás registrado?</p>
+            <a href="register.php">REGISTRARME AHORA </a>
 
   </body>
+  <style>
+  @media (min-width: 600px) {
+    .site-title{
+      width: 38vw;
+     }
+
+    .site-title h1 {
+     font-size: 10vw;
+    }
+
+    .site-title h3 {
+      font-size: 1.7vw;
+      margin-bottom: 11vh;
+
+  }
+
+  .registrar{
+    font-size: 3vw;
+
+  }
+
+  .description p{
+    font-size: 1.7vw;
+  }
+
+  @media (min-width: 1024px) {
+    .site-title{
+      width: 30vw;
+     }
+
+    .site-title h1 {
+     font-size: 8vw;
+    }
+
+    .site-title h3 {
+      font-size: 1vw;
+      margin-bottom: 18vh;
+
+  }
+
+  .registrar{
+    font-size: 2vw;
+    margin-bottom: 3vh;
+
+  }
+
+  .description p{
+    font-size: 1.5vw;
+  }
+  </style>
 </html>
--- register.php
+++ register.php
@@ -2,12 +2,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
     <link rel="stylesheet" href="css/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Crea tu cuenta</title>
   </head>
-  <body>
+  <body class= "galaxy">
     <div class="container" style="margin-top:30px; margin-bottom: 30px;">
       <div class="row justify-content-center">
       <div class= "site-title">
@@ -17,8 +17,8 @@
 
       <form method="post" enctype="multipart/form-data">
 					<div class="row">
-						<div class="col-sm-10">
-              <h2>REGISTRARME</h2>
+						<div class="col-10 formulario">
+              <h2 class= "registrar" >REGISTRARME</h2>
               <div class="form-group">
                 <input type="text" name="user" class="form-control" placeholder="Nombre de Usuario">
 
@@ -40,7 +40,7 @@
 								<input type="password" name="pass-repeat" class="form-control" placeholder="Repita su contraseña">
 
               </div>
-              <div class="col-12">
+              <div class="col-10 reg-button">
 
 							<button type="submit" class="btn btn-primary">Registrarse</button>
 						</div>
@@ -52,6 +52,48 @@
         </div>
       </div>
 
+      <style>
+      @media (min-width: 600px) {
+        .site-title{
+        	width: 38vw;
+         }
+
+        .site-title h1 {
+       	 font-size: 10vw;
+        }
+
+        .site-title h3 {
+          font-size: 1.7vw;
+          margin-bottom: 11vh;
+
+      }
+
+      .registrar{
+     		font-size: 3vw;
+
+      }
+
+      @media (min-width: 1024px) {
+        .site-title{
+          width: 30vw;
+         }
+
+        .site-title h1 {
+         font-size: 8vw;
+        }
+
+        .site-title h3 {
+          font-size: 1vw;
+          margin-bottom: 18vh;
+
+      }
+
+      .registrar{
+        font-size: 2vw;
+        margin-bottom: 3vh;
+
+      }
+      </style>
 
   </body>
 </html>
